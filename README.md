a1a5c7988990dca28fe3fee7f049996c0c5f3404
naq219 <naq219@gmail.com>
2021-09-28 10:39:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-jtadiz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-jtadiz)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>xin chao ne</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ff00ff;
+  margin-top: 60px;
+}
+
+p {
+  color: #13a725;
+}
+</style>
